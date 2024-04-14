23b9eb9b82d340b4b430bd0eeb1ded291f7361c1
adityaVelhal <aditya.v.velhal@gmail.com>
2024-04-14 17:29:44 +0530

    styled genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,16 +1,24 @@
-import { Text } from '@chakra-ui/react';
-import useData from '../hooks/useData';
-import Genre from '../models/Genre';
+import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
 import useGenres from '../hooks/useGenre';
+import getCroppedImageUrl from '../services/image-url.';
 
 const GenreList = () => {
     const { data, error, isLoading } = useGenres();
     return (
-        <>
+        <List>
             {data.map((genre) => (
-                <Text>{genre.name}</Text>
+                <ListItem key={genre.id} paddingY="5px">
+                    <HStack>
+                        <Image
+                            boxSize="32px"
+                            borderRadius={8}
+                            src={getCroppedImageUrl(genre.image_background)}
+                        />
+                        <Text fontSize='large'> {genre.name} </Text>
+                    </HStack>
+                </ListItem>
             ))}
-        </>
+        </List>
     );
 };
 
