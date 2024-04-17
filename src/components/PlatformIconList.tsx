fc0ada7fa5e0480c016c1500da0ab276f3b9a0ab
adityaVelhal <aditya.v.velhal@gmail.com>
2024-04-17 20:43:41 +0530

    deployment ready

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -9,9 +9,9 @@ import {
 import { MdPhoneIphone } from 'react-icons/md';
 import { SiNintendo } from 'react-icons/si';
 import { BsGlobe } from 'react-icons/bs';
-import { Platform } from '../models/Games';
 import { HStack, Icon, Text } from '@chakra-ui/react';
 import { IconType } from 'react-icons';
+import Platform from './../models/Platform';
 
 interface Props {
     platforms: Platform[];
