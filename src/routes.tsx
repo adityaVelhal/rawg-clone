4f1578c0ab5b6f6f29a481548711b9c47b12c03d
adityaVelhal <aditya.v.velhal@gmail.com>
2024-05-04 01:12:36 +0530

    Handled errors in routing

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,11 +3,13 @@ import NavBar from './components/NavBar';
 import Layout from './pages/Layout';
 import GameDetailsPage from './pages/GameDetailsPage';
 import HomePage from './pages/HomePage';
+import ErrorPage from './pages/ErrorPage';
 
 const route = createBrowserRouter([
     {
         path: '/',
         element: <Layout />,
+        errorElement: <ErrorPage />,
         children: [
             { index: true, element: <HomePage /> },
             { path: 'games/:id', element: <GameDetailsPage /> },
