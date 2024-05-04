b7f44750aa50cee4f3f9a00da01accc22a082c0c
adityaVelhal <aditya.v.velhal@gmail.com>
2024-05-04 20:58:09 +0530

    Fix the nav bar

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,4 +1,4 @@
-import { Heading, Spinner } from '@chakra-ui/react';
+import { Box, Heading, SimpleGrid, Spinner } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import ExpandableText from '../components/ExpandableText';
 import GameAttributes from '../components/GameAttributes';
@@ -13,13 +13,17 @@ const GameDetailsPage = () => {
     if (isLoading) return <Spinner />;
     if (error || !game) throw error;
     return (
-        <>
-            <Heading>{game.name}</Heading>
-            <ExpandableText>{game.description_raw}</ExpandableText>
-            <GameAttributes game={game} />
-            <GameTrailer id={game.slug} />
-            <GameScreenshots gameId={game.id} />
-        </>
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+            <Box>
+                <Heading>{game.name}</Heading>
+                <ExpandableText>{game.description_raw}</ExpandableText>
+                <GameAttributes game={game} />
+            </Box>
+            <Box>
+                <GameTrailer id={game.slug} />
+                <GameScreenshots gameId={game.id} />
+            </Box>
+        </SimpleGrid>
     );
 };
 
